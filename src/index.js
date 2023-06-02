d59574d17290a6b344cdcc9bcc2e94b4feea75b6
nzxf <122195155+nzxf@users.noreply.github.com>
2023-06-03 00:01:55 +0700

    Sync the delete button

--- src/index.js
+++ src/index.js
@@ -7,48 +7,45 @@ import { functions } from './allFunctions';
 const elMaker = functions.elMaker;
 const makeNavbar = functions.makeNavbar;
 
-const content = document.querySelector('.content');
+const childRemover = (parent) => {
+  while (parent.hasChildNodes()) {
+    parent.removeChild(parent.children[0]);
+  }
+};
 
+// DELETE BUTTON MAKER
+const addDeleteButton = (data, parent, targetEl, targetArr, targetIndex) => {
+  const deleteContainer = elMaker('div', parent, '', 'delete-container');
+  const deleteButton = elMaker('button', deleteContainer, '', 'delete-button');
+  deleteButton.addEventListener('mouseup', function () {
+    targetArr.splice(targetIndex, 1); // DELETE THE PROJECT
+    childRemover(targetEl); // CLEAR MAINBOARD
+    fillData(data, document.querySelector('.main-body')); // REFILL MAINBOARD WITH NEW DATA
+  });
+};
+
+const content = document.querySelector('.content');
 makeNavbar(content);
 
 const fillData = (data, parent) => {
+  // console.log(data);
   for (let i = 0; i < data.length; i++) {
     const projectContainer = elMaker('div', parent, '', 'project-container');
-    const projectName = elMaker(
-      'div',
-      projectContainer,
-      data[i].name.toUpperCase(),
-      'project-name'
-    );
+    // DELETE BUTTON (PROJECT)
+    addDeleteButton(data, projectContainer, parent, data, i);
+    const projectName = elMaker('div', projectContainer, data[i].name.toUpperCase(), 'project-name');
     for (let j = 0; j < data[i].content.length; j++) {
-      // let data = data[i].content;
       const listContainer = elMaker('div', projectContainer, '', 'list-container');
-      const listTitle = elMaker(
-        'div',
-        listContainer,
-        data[i].content[j].title,
-        'list-title'
-      );
-      const listText = elMaker(
-        'div',
-        listContainer,
-        data[i].content[j].text,
-        'list-text'
-        );
-      }
-    elMaker('div', projectContainer, 'Add a list', 'add-list-button')
+      // DELETE BUTTON (LIST)
+      addDeleteButton(data, listContainer, parent, data[i].content, j);
+      const listTitle = elMaker('div', listContainer, data[i].content[j].title, 'list-title');
+      const listText = elMaker('div', listContainer, data[i].content[j].text, 'list-text');
+    }
+    elMaker('div', projectContainer, 'Add a list', 'add-list-button');
   }
   const addProjectContainer = elMaker('div', parent, '', 'add-project-container');
-  elMaker('div', addProjectContainer, 'Add a project', 'add-project-button')
+  elMaker('div', addProjectContainer, 'Add a project', 'add-project-button');
 };
 
 const mainBody = elMaker('div', content, '', 'main-body');
 fillData(allProjects, mainBody);
-
-// const makeButtons = (parent) => {
-//   const butttonContainer = elMaker('div', parent, '', 'button-container')
-//   const edit =  elMaker('button', butttonContainer, '', 'edit-button')
-//   const del = elMaker('button', butttonContainer, '', 'delete-button')
-// }
-
-makeButtons(mainBody)
